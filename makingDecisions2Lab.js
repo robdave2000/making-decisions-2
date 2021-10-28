2648586403731843c592451e9113ec6d7a74f7fb
Robert Davis <robdave22@yahoo.com>
2021-10-28 14:51:41 -0400

    Problem 5 works

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -67,7 +67,26 @@ var score = 74
 */
 
 //Code Here
-
+if (score >= 90)
+{
+  console.log("A");
+}
+else if (score < 90 && score >= 80)
+{
+  console.log("B");
+}
+else if (score < 80 && score >=70)
+{
+  console.log("C");
+}
+else if (score < 70 && score >= 60)
+{
+  console.log("D");
+}
+else
+{
+  console.log("F");
+}
 
 ////////// Intermediate Problems //////////
 
