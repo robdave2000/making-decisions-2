0b122f3269c80515d17c2e90869d65a97c614d17
Robert Davis <robdave22@yahoo.com>
2021-10-28 14:42:18 -0400

    Problem 3 working

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -29,7 +29,10 @@ var family = ['Tyler', 'Jordan', 'Ryan', 'Alice', 'Ireland'];
 */
 
 //Code Here
-
+for (let i = 0; i < family.length; i++)
+{
+  console.log(family[i]);
+}
 
 
 ////////// PROBLEM 4 //////////
